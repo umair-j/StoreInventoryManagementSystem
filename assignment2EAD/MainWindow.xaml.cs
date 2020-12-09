2f04e2a1f8c96a41e8a771af0c13721fcab4ea70
umair-j <58130030+umair-j@users.noreply.github.com>
2020-12-10 04:37:46 +0500

    Add project files.

---
+++ assignment2EAD/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using assignment2EAD.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace assignment2EAD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void adminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            adminMain adMain = new adminMain();
+            adMain.Show();
+        }
+
+        private void customerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            customerMain cMain = new customerMain();
+            cMain.Show();
+        }
+    }
+}
---
+++ assignment2EAD/View/adminMain.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace assignment2EAD.View
+{
+    /// <summary>
+    /// Interaction logic for adminMain.xaml
+    /// </summary>
+    public partial class adminMain : Window
+    {
+        public adminMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
